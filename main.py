3cb0d427ce75a2d2401c3ab8bc7e1650e9a2d294
takeru.abe <takeru.abe@gaiax.com>
2022-01-10 12:24:38 +0900

    prediction for test dataset

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import mglearn as mglearn
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+X, y = mglearn.datasets.make_forge()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+clf = KNeighborsClassifier(n_neighbors=3)
+clf.fit(X_train, y_train)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Test set predictions: {}".format(clf.predict(X_test)))
